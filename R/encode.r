95085534a60df10719b66177354712b44cebc777
Thomas Levine <_@thomaslevine.com>
2016-02-24 23:00:21 +0000

    separate helpers

---
+++ R/encode.r
@@ -0,0 +1,6 @@
+U8 <- function(x) {
+  if (any(is.na(x)) || min(x) < -1 || max(x) > 1)
+    stop('You must normalize and remove NAs.')
+  else
+    as.raw(128 + floor(x*127.5))
+}
---
+++ R/synth.r
@@ -0,0 +1,17 @@
+#' @example scale.wave(rnorm(100))
+scale.wave <- function(x)
+  -1 + 2 * ((x - min(x)) / (max(x) - min(x)))
+
+#' @example
+#'   f <- function(t) sin(440 * t * 2 * pi / 8000)
+#'   wapply(1.5, f)
+#'
+#'   # Without wapply
+#'   A4 <- sin(440 * (1:12000) * 2 * pi / 8000)
+#'   # With wapply
+#'   A4.fun <- function(seconds) sin(440 * seconds * 2 * pi)
+#'   A4 <- wapply(1.5, A4.fun)
+wapply <- function(seconds, f, bitrate=8000) {
+  n <- round(seconds*bitrate)
+  f((1:n)/n)
+}
