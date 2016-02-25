bac523a6b5f060797af1a52bbb15f09b33b400af
Thomas Levine <_@thomaslevine.com>
2016-02-25 12:29:22 +0000

    looping semes to work

--- R/loop.r
+++ R/loop.r
@@ -1,21 +1,24 @@
-# BITRATE = 8000 # per second
-# BUFFER = 1/12 # seconds
 options(digits.secs = 9)
 
 #' @export
 loop.service <- function(params, next.sample=NULL) {
   if (is.null(next.sample)) next.sample <- params$prev.sample
-  next.time <- params$prev.time + length(next.sample) / params$bitrate
 
-  if (next.time - (params$bitrate * params$buffer) < Sys.time()) {
+  next.time <- params$prev.time + length(next.sample) / params$bitrate
+  almost.next.time <- next.time - params$buffer
+
+  if (almost.next.time < Sys.time()) {
     params$play(next.sample, params$out)
-    params$prev.time <- next.time
+    params$prev.time <- max(next.time, Sys.time())
   }
   params
 }
 
+
+# bitrate = 8000 # per second
+# buffer = 1/12 # seconds
 #' @export
-loop.new <- function(out, .bitrate=8000, .buffer=1/12, .play=play) {
+loop.new <- function(out, .bitrate=8000, .buffer=1/2, .play=play) {
   silence <- rep(0, .bitrate/2) # half second of silence
   params <- list(bitrate=.bitrate, buffer=.buffer, play=.play,
                  out=out, prev.time=Sys.time(), prev.sample=silence)
