5b174ac39b2e0f64f62e69d8d6a2fd9c98489a85
Thomas Levine <_@thomaslevine.com>
2016-02-25 08:55:03 +0000

    try not blocking

--- R/dsp.r
+++ R/dsp.r
@@ -1,7 +1,11 @@
 .env <- new.env()
-dsp <- function() {
+dsp <- function(force.new=FALSE) {
+  if (force.new && 'dsp' %in% ls(.env)) {
+    close(get('dsp', .env))
+    remove('dsp', envir=.env)
+  }
   if (!('dsp' %in% ls(.env)))
-    assign('dsp', file('/dev/dsp', open = 'wb'), .env)
+    assign('dsp', file('/dev/dsp', open = 'wb', blocking=FALSE), .env)
   get('dsp', .env)
 }
 play <- function(x, dsp) writeBin(encode(x), dsp)
