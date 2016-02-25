80b799bf5e3b1a5ed40ee16694384ad0853a27bb
Thomas Levine <_@thomaslevine.com>
2016-02-25 10:52:00 +0000

    Automatic commit with j

--- dspbuffer.sh
+++ dspbuffer.sh
@@ -4,6 +4,5 @@ set -e
 PORT=22222
 BUFFER=8000
 
-nc -u -l -p $PORT |
-  dd ibs=$BUFFER iflag=fullblock oflag=dsync >
-  /dev/dsp
+nc -l -p $PORT |
+  dd ibs=$BUFFER iflag=fullblock oflag=dsync > /dev/dsp
