68ad3f69a6f5635b70ee11030f365be93af9b7f2
Thomas Levine <_@thomaslevine.com>
2016-02-29 22:34:33 +0000

    consolidate files

--- R/dsp.r
+++ R/dsp.r
@@ -1,5 +1,16 @@
+#' Play a sound
+#' @export
+#' @param x The sound, as a numeric vector with elements between -1 and 1
+#'   and with bitrate of 8000, that is, 8000 elements per second.
 play <- function(x) {
   dsp <- file('/dev/dsp', open = 'a+b')
   writeBin(encode(x), dsp)
   close(dsp)
+}
+
+encode <- function(x) {
+  if (any(is.na(x)) || min(x) < -1 || max(x) > 1)
+    stop('You must normalize and remove NAs.')
+  else
+    as.raw(128 + floor(x*127.5))
 }
--- R/encode.r
+++
@@ -1,6 +0,0 @@
-encode <- function(x) {
-  if (any(is.na(x)) || min(x) < -1 || max(x) > 1)
-    stop('You must normalize and remove NAs.')
-  else
-    as.raw(128 + floor(x*127.5))
-}
